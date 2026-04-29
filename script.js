e1b4e20ffdd3d7561963cf524d6c9e3001544390
kjasminss <momonomoochuwa@gmail.com>
2026-04-29 20:59:44 +0800

    init website

---
+++ script.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll(".section");
+const navItems = document.querySelectorAll(".nav-item");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (window.scrollY >= sectionTop - sectionHeight / 2) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navItems.forEach(item => {
+    item.classList.remove("active");
+    if (item.getAttribute("href") === "#" + current) {
+      item.classList.add("active");
+    }
+  });
+});
